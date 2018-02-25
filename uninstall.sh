7375618d6a94655eabde2c94fa815bf9ff02a363
midnightchip <34963874+midnightchip@users.noreply.github.com>
2018-02-24 20:03:14 -0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 echo "Removing Ext3nder"
-rm -rf Applications/Extender.app 
+rm -rf /Applications/Extender.app 
 rm /bootstrap/Library/SBInject/Ext3nder_SpringBoard.dylib 
 rm /bootstrap/Library/SBInject/Ext3nder_SpringBoard.plist 
 rm -rf /Library/Switches/Ext3nderSwitch.bundle
